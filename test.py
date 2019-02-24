40a6a21a7b8f1685b458e9e4700272380028e780
akashpalrecha <akashpalrecha@gmail.com>
2019-02-24 22:48:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,5 @@ import multichannel_resnet
 from multichannel_resnet import get_arch as Resnet
 
 resnet34_4_channel = Resnet(34, 4)
-
 model = resnet34_4_channel(True)
 print("New input channels : ", model.conv1.in_channels)
